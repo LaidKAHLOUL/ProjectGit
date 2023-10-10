0c92c28e6055db07ac04749d4a80c7019f5ecaaa
HIGH TECH <HIGH TECH@192.168.0.109>
2023-10-10 18:47:17 +0100

    adding z

---
+++ src/MainClass.java
@@ -0,0 +1,12 @@
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=0;
+		int y=0;
+		System.out.print("x= "+x+ "  y=  "+y);
+
+	}
+
+}
